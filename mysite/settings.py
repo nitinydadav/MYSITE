0fffa10113c08740ee17b810370ddb040ad01f62
nitinydadav <nitin.yad005@gmail.com>
2020-05-09 13:17:54 +0530

    first project

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
